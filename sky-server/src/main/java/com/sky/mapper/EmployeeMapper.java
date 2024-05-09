a18ac6a2a758a8df38ebfce3efc816a882bb528d
Erkang Xia <xiaerkang@icloud.com>
2024-05-09 16:57:09 -0400

    custom annotation:autoFill and aspect

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -23,6 +25,7 @@ public interface EmployeeMapper {
      * @param employee
      */
     @Insert(value = "insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user,status) values( #{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser},#{status})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /**分页查询，因为是动态sql 用注解不方便，写到配置文件里
@@ -35,6 +38,7 @@ public interface EmployeeMapper {
      * 启用禁用员工account
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**查找员工by Id
