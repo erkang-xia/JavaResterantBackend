6f61830d70f458f81263e2c3d5022f3f607552a9
Erkang Xia <xiaerkang@icloud.com>
2024-05-23 12:37:18 -0400

    top10

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,11 +1,13 @@
 package com.sky.mapper;
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -59,4 +61,6 @@ public interface OrderMapper {
     Double sumByDate(Map map);
 
     Integer countByDate(Map map);
+
+    List<GoodsSalesDTO> getTop10(LocalDateTime begin, LocalDateTime end);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,10 +1,12 @@
 package com.sky.service.impl;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -144,6 +147,26 @@ public class ReportServiceImpl implements ReportService {
                 .totalOrderCount(totalCount)
                 .validOrderCount(validCount)
                 .orderCompletionRate(orderCompleteRate)
+                .build();
+    }
+
+    /**top10
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+        LocalDateTime start = LocalDateTime.of(begin, LocalTime.MIN);
+        LocalDateTime finish = LocalDateTime.of(end, LocalTime.MAX);
+        List<GoodsSalesDTO> top10 = orderMapper.getTop10(start,finish);
+        List<String> names = top10.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        List<Integer> number = top10.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+
+
+        return SalesTop10ReportVO.builder()
+                .nameList(StringUtils.join(names,","))
+                .numberList(StringUtils.join(number,","))
                 .build();
     }
 
