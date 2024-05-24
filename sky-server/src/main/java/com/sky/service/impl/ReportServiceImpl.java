b22a00fda1b478ebbddf7a9264ef12abf4e436d6
Erkang Xia <xiaerkang@icloud.com>
2024-05-24 11:29:19 -0400

    excel export

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -5,15 +5,20 @@ import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
-import com.sky.vo.OrderReportVO;
-import com.sky.vo.SalesTop10ReportVO;
-import com.sky.vo.TurnoverReportVO;
-import com.sky.vo.UserReportVO;
+import com.sky.service.WorkspaceService;
+import com.sky.vo.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -32,6 +37,8 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    private WorkspaceService workspaceService;
 
     /**营业额统计
      *
@@ -168,6 +175,66 @@ public class ReportServiceImpl implements ReportService {
                 .nameList(StringUtils.join(names,","))
                 .numberList(StringUtils.join(number,","))
                 .build();
+    }
+
+    /**数据导出
+     *
+     * @param response
+     */
+    public void exportBusinessData(HttpServletResponse response) {
+        LocalDate begin = LocalDate.now().minusDays(30);
+        LocalDate end = LocalDate.now().minusDays(1);
+
+        //查询数据
+        BusinessDataVO businessData = workspaceService.getBusinessData(LocalDateTime.of(begin, LocalTime.MIN), LocalDateTime.of(end, LocalTime.MAX));
+
+        //写入Excel
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("template/运营数据报表模板.xlsx");
+
+        try{
+            XSSFWorkbook excel = new XSSFWorkbook(in);
+
+            //填充数据
+            XSSFSheet sheet = excel.getSheetAt(0);
+            //时间
+            sheet.getRow(1).getCell(1).setCellValue("时间：" + begin + " 至 " + end);
+            //数据
+            XSSFRow row = sheet.getRow(3);
+            row.getCell(2).setCellValue(businessData.getTurnover());
+            row.getCell(4).setCellValue(businessData.getOrderCompletionRate());
+            row.getCell(6).setCellValue(businessData.getNewUsers());
+
+            row = sheet.getRow(4);
+            row.getCell(2).setCellValue(businessData.getValidOrderCount());
+            row.getCell(4).setCellValue(businessData.getUnitPrice());
+
+
+            for(int i = 0; i<30;i++){
+                LocalDate date = begin.plusDays(i);
+                BusinessDataVO dailyBusinessData = workspaceService.getBusinessData(LocalDateTime.of(date, LocalTime.MIN), LocalDateTime.of(date, LocalTime.MAX));
+                row = sheet.getRow(7+i);
+                row.getCell(1).setCellValue(date.toString());
+                row.getCell(2).setCellValue(dailyBusinessData.getTurnover());
+                row.getCell(3).setCellValue(dailyBusinessData.getValidOrderCount());
+                row.getCell(4).setCellValue(dailyBusinessData.getOrderCompletionRate());
+                row.getCell(5).setCellValue(dailyBusinessData.getUnitPrice());
+                row.getCell(6).setCellValue(dailyBusinessData.getNewUsers());
+            }
+
+            //通过输出流把excel下载到浏览器
+            ServletOutputStream outputStream = response.getOutputStream();
+            excel.write(outputStream);
+
+
+            outputStream.close();
+            excel.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
     }
 
     private Integer getOrderCount(LocalDateTime begin, LocalDateTime end, Integer status) {
